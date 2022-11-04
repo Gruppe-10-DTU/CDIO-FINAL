70f31a376718fd91268efca361e14b2899411a1a
Philip Astrup Cramer <s224319@dtu.dk>
2022-11-04 11:29:55 +0100

    added card type to enumerator

--- src/main/chanceCards/DeckComposition.java
+++ src/main/chanceCards/DeckComposition.java
@@ -1,34 +1,40 @@
 package chanceCards;
 
 public enum DeckComposition {
-    CHARACTER_SPECIFIC_1,
-    CHARACTER_SPECIFIC_2,
-    CHARACTER_SPECIFIC_3,
-    CHARACTER_SPECIFIC_4,
+    CHARACTER_SPECIFIC_1("CharacterSpecific"),
+    CHARACTER_SPECIFIC_2("CharacterSpecific"),
+    CHARACTER_SPECIFIC_3("CharacterSpecific"),
+    CHARACTER_SPECIFIC_4("CharacterSpecific"),
 
-    MOVE_TO_COLOUR_1,
-    MOVE_TO_COLOUR_2,
-    MOVE_TO_COLOUR_3,
-    MOVE_TO_COLOUR_4,
-    MOVE_TO_COLOUR_5,
-    MOVE_TO_COLOUR_6,
-    MOVE_TO_COLOUR_7,
+    MOVE_TO_COLOUR_1("MoveToColour"),
+    MOVE_TO_COLOUR_2("MoveToColour"),
+    MOVE_TO_COLOUR_3("MoveToColour"),
+    MOVE_TO_COLOUR_4("MoveToColour"),
+    MOVE_TO_COLOUR_5("MoveToColour"),
+    MOVE_TO_COLOUR_6("MoveToColour"),
+    MOVE_TO_COLOUR_7("MoveToColour"),
 
-    MOVE_TO_FIELD_1,
-    MOVE_TO_FIELD_2,
-    MOVE_TO_FIELD_3,
+    MOVE_TO_FIELD_1("MoveToField"),
+    MOVE_TO_FIELD_2("MoveToField"),
+    MOVE_TO_FIELD_3("MoveToField"),
 
-    GET_OUT_OF_JAIL,
+    GET_OUT_OF_JAIL("GetOutOfJail"),
 
-    CHANGE_BALANCE_1,
-    CHANGE_BALANCE_2,
-    CHANGE_BALANCE_3,
+    CHANGE_BALANCE_1("ChangeBalance"),
+    CHANGE_BALANCE_2("ChangeBalance"),
+    CHANGE_BALANCE_3("ChangeBalance"),
 
-    CHOICE_1,
+    CHOICE_1("Choice"),
 
-    MOVE_X_STEPS;
+    MOVE_X_STEPS("MoveXSteps");
 
-    DeckComposition(){
+    private final String type;
+
+    DeckComposition(String Type){
+        this.type = Type;
     }
 
+    public String getType() {
+        return type;
+    }
 }
