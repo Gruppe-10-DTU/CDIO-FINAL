8eaf883638cf6d0b9df19036531bc797c2351cb3
Philip Astrup Cramer <s224319@dtu.dk>
2022-11-04 11:16:26 +0100

    created Super-constructor

--- src/main/chanceCards/ChanceCard.java
+++ src/main/chanceCards/ChanceCard.java
@@ -2,4 +2,15 @@ package chanceCards;
 
 public abstract class ChanceCard {
     protected String name;
+    protected String description;
+
+    /**
+     * Super constructor
+     * @param Name        Must match a key in the language hashmap
+     * @param Description Must be imported from the language hashmap
+     */
+    public ChanceCard(String Name, String Description){
+        this.name = Name;
+        this.description = Description;
+    }
 }
