9e80b43a38c25a4a0196ad7ecc6154bf97b6444e
Philip Astrup Cramer <s224319@dtu.dk>
2022-11-04 09:39:34 +0100

    created shuffle method using Fisher-Yates shuffle algorithm

--- src/main/chanceCards/Deck.java
+++ src/main/chanceCards/Deck.java
@@ -7,4 +7,22 @@ public class Deck {
     public Deck(){
 
     }
+
+    /**
+     * Implementation of the Fisher-Yates shuffle
+     * Source: <a href="https://en.wikipedia.org/wiki/Fisher-Yates_shuffle#The_modern_algorithm">Wikipedia</a>
+     */
+    private void shuffle(){
+        //Iterates from the back of the deck
+        for(int position = deck.length -1; position > 0; position--){
+
+            //Generates a random number such that 0 <= randomNum <= position
+            int randomNum = (int) (Math.random() * (position + 1));
+
+            //Swaps the card at the random index and the iterated position
+            ChanceCard cardHolder = deck[position];
+            deck[position] = deck[randomNum];
+            deck[randomNum] = cardHolder;
+        }
+    }
 }
