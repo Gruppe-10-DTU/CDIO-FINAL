25486c449cf6f8301059bba563e378f86df12c2b
Philip Astrup Cramer <s224319@dtu.dk>
2022-11-04 11:17:19 +0100

    autogenerated default constructors based of super constructor

--- src/main/chanceCards/MoveToField.java
+++ src/main/chanceCards/MoveToField.java
@@ -3,4 +3,13 @@ package chanceCards;
 public class MoveToField extends ChanceCard{
 
     private String fieldName;
+
+    /**
+     * Constructor for the Chancecards that move the player to a specific field.
+     * @param Name        Must match a key in the language hashmap
+     * @param Description Must be imported from the language hashmap
+     */
+    public MoveToField(String Name, String Description) {
+        super(Name, Description);
+    }
 }
