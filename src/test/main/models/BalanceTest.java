3a844732180159525d03f9aa2d0f52f1e22b9a8a
NilasThoegersen <nilas96@gmail.com>
2022-11-07 11:38:53 +0100

    Added balance test

---
+++ src/test/main/models/BalanceTest.java
@@ -0,0 +1,19 @@
+package main.models;
+
+import junit.framework.TestCase;
+
+public class BalanceTest extends TestCase {
+
+    public void testGetBalance() {
+        Balance balance = new Balance(20);
+        assertEquals(20, balance.getBalance());
+    }
+
+    public void testSetBalance() {
+        Balance balance = new Balance(20);
+        balance.setBalance(10);
+        assertEquals(10,balance.getBalance());
+        balance.setBalance(2);
+        assertEquals(2, balance.getBalance());
+    }
+}
