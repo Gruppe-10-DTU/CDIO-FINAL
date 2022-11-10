6b46d890bd2ae283f1dea5352dce2fbb85ddcd13
Philip Astrup Cramer <s224319@dtu.dk>
2022-11-10 10:19:43 +0100

    added move value and drawAgain

--- src/main/java/chanceCards/Choice.java
+++ src/main/java/chanceCards/Choice.java
@@ -2,12 +2,17 @@ package chanceCards;
 
 public class Choice extends ChanceCard{
 
+    private final int move;
+    private final boolean drawAgain;
+
     /**
      * Constructor for the Chancecards that leave the player with a choice
      * @param Name        Must match a key in the language hashmap
      * @param Description Must be imported from the language hashmap
      */
-    public Choice(String Name, String Description ) {
+    public Choice(String Name, String Description, int Move, boolean DrawAgain) {
         super(Name, Description);
+        this.move = Move;
+        this.drawAgain = DrawAgain;
     }
 }
