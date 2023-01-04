37263bcd00e40d63466820622343d5ea485b27cc
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-04 09:45:20 +0100

    added attributes and constructor to grant class

--- src/main/java/models/chanceCards/Grant.java
+++ src/main/java/models/chanceCards/Grant.java
@@ -2,19 +2,19 @@ package models.chanceCards;
 
 public class Grant extends ChanceCard{
 
-    private int bonus;
-    private int netWorth;
+    private final int BONUS;
+    private final int NET_WORTH;
     public Grant(String NAME, String Description, int MaximumPlayerWorth, int AwardsBonus) {
         super(NAME, Description);
-        this.bonus = AwardsBonus;
-        this.netWorth = MaximumPlayerWorth;
+        this.BONUS = AwardsBonus;
+        this.NET_WORTH = MaximumPlayerWorth;
     }
 
-    public int getBonus() {
-        return bonus;
+    public int getBONUS() {
+        return BONUS;
     }
 
-    public int getNetWorth() {
-        return netWorth;
+    public int getNET_WORTH() {
+        return NET_WORTH;
     }
 }
