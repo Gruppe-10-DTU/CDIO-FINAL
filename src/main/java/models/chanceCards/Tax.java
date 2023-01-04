e50e809ff4cd4940e1efd1920ac8d4af69530edd
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-04 09:46:03 +0100

    fixed attributes and constructor to Tax class

--- src/main/java/models/chanceCards/Tax.java
+++ src/main/java/models/chanceCards/Tax.java
@@ -2,21 +2,21 @@ package models.chanceCards;
 
 public class Tax extends ChanceCard {
 
-    private int houseTax;
-    private int hotelTax;
+    private final int HOUSE_TAX;
+    private final int HOTEL_TAX;
 
 
     public Tax (String Name, String Description, int Value_1, int Value_2){
         super(Name, Description);
-        this.houseTax = Value_1;
-        this.hotelTax = Value_2;
+        this.HOUSE_TAX = Value_1;
+        this.HOTEL_TAX = Value_2;
     }
 
-    public int getHouseTax() {
-        return houseTax;
+    public int getHOUSE_TAX() {
+        return HOUSE_TAX;
     }
 
-    public int getHotelTax() {
-        return hotelTax;
+    public int getHOTEL_TAX() {
+        return HOTEL_TAX;
     }
 }
