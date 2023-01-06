cbd10c00b9841eb39249c61279358a6481e80590
A-bjoern <113096228+A-bjoern@users.noreply.github.com>
2023-01-06 12:51:46 +0100

    AcceptanceTest (AK7) for displaying player balance.

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -4,12 +4,9 @@ import controllers.CheatDiceHolder;
 import controllers.FieldController;
 import controllers.GameController;
 import controllers.PlayerController;
-import gui_main.GUI;
 import models.Language;
-import models.Player;
 import models.chanceCards.Deck;
 import models.dto.GameStateDTO;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -17,44 +14,39 @@ import ui.GUIController;
 
 @Disabled("Only used for acceptance test")
 public class AcceptanceTest {
-    private GameController gameController;
-    private GameStateDTO gameState;
-    private PlayerController playerController;
-    private FieldController fieldController;
+    private GameController gc;
+    private GameStateDTO gs;
+    private PlayerController pc;
+    private FieldController fc;
     private Language language;
-    private CheatDiceHolder diceHolder;
+    private CheatDiceHolder dH;
     private Deck deck;
-    private GUIController guiController;
+    private GUIController gui;
+
+
 
     @BeforeEach
-    public void beforeAll(){
+    public void beforeAll() {
         language = new Language();
-        fieldController = new FieldController(language);
-        guiController = new GUIController(fieldController.getFieldList());
-        gameState = new GameStateDTO(guiController);
-
-        gameState.setFieldController(fieldController);
-        diceHolder = new CheatDiceHolder(2);
-        gameState.setDiceHolder(diceHolder);
-        playerController = new PlayerController();
-        gameState.setPlayerController(playerController);
-        deck = new Deck(language);
-        gameController = new GameController(gameState, language, deck);
-    public void beforeEach(){
-        gui = new GUIController();
-        gc = new GameController();
+        fc = new FieldController(language);
+        gui = new GUIController(fc.getFieldList());
         gs = new GameStateDTO(gui);
-        pc = new PlayerController();
-        fc = new FieldController(dansk);
 
+        gs.setFieldController(fc);
+        dH = new CheatDiceHolder(2);
+        gs.setDiceHolder(dH);
+        pc = new PlayerController();
+        gs.setPlayerController(pc);
+        deck = new Deck(language);
+        gc = new GameController(gs, language, deck);
     }
     @Test
     public void AK5_6(){
-        diceHolder.setRolls(5,5);
-        playerController.addPlayer(0, "car", "test",0);
-        guiController.setPlayers(playerController.getPlayers());
-        diceHolder.setNextRoll(10);
-        gameController.TakeTurn(playerController.getPlayerById(0));
+        dH.setRolls(5,5);
+        pc.addPlayer(0, "car", "test",0);
+        gui.setPlayers(pc.getPlayers());
+        dH.setNextRoll(10);
+        gc.TakeTurn(pc.getPlayerById(0));
     }
 
 
@@ -62,7 +54,7 @@ public class AcceptanceTest {
     public void AK7(){
         pc.addPlayer(0,"UFO","moneyTest",2);
         pc.addPlayer(1,"UFO","dummy1",2);
-        pc.addPlayer(1,"UFO","dummy2",2);
+        gui.setPlayers(pc.getPlayers());
         gc.TakeTurn(pc.getPlayerById(0));
 
     }
