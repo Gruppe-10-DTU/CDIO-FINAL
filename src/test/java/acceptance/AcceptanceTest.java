f0c24be907ae7b975a98e5ada1983575a33244cd
NilasThoegersen <nilas96@gmail.com>
2023-01-06 12:40:25 +0100

    Added code for acceptance test 5 and 6

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -1,6 +1,13 @@
 package acceptance;
 
+import controllers.CheatDiceHolder;
+import controllers.FieldController;
 import controllers.GameController;
+import controllers.PlayerController;
+import gui_main.GUI;
+import models.Language;
+import models.Player;
+import models.chanceCards.Deck;
 import models.dto.GameStateDTO;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,12 +19,34 @@ import ui.GUIController;
 public class AcceptanceTest {
     private GameController gameController;
     private GameStateDTO gameState;
+    private PlayerController playerController;
+    private FieldController fieldController;
+    private Language language;
+    private CheatDiceHolder diceHolder;
+    private Deck deck;
+    private GUIController guiController;
+
     @BeforeEach
     public void beforeAll(){
-        GUIController guiController = new GUIController();
+        language = new Language();
+        fieldController = new FieldController(language);
+        guiController = new GUIController(fieldController.getFieldList());
+        gameState = new GameStateDTO(guiController);
+
+        gameState.setFieldController(fieldController);
+        diceHolder = new CheatDiceHolder(2);
+        gameState.setDiceHolder(diceHolder);
+        playerController = new PlayerController();
+        gameState.setPlayerController(playerController);
+        deck = new Deck(language);
+        gameController = new GameController(gameState, language, deck);
     }
     @Test
     public void AK5_6(){
-        System.out.println("Test");
+        diceHolder.setRolls(5,5);
+        playerController.addPlayer(0, "car", "test",0);
+        guiController.setPlayers(playerController.getPlayers());
+        diceHolder.setNextRoll(10);
+        gameController.TakeTurn(playerController.getPlayerById(0));
     }
 }
