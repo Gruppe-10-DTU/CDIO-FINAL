0e25c0a649fb3819005942fd14093f1b51079a6a
A-bjoern <113096228+A-bjoern@users.noreply.github.com>
2023-01-06 12:43:22 +0100

    Developent of AK7

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -1,23 +1,48 @@
 package acceptance;
 
+import controllers.FieldController;
 import controllers.GameController;
+import controllers.PlayerController;
+import models.Language;
 import models.dto.GameStateDTO;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ui.GUIController;
+import controllers.GUIControllerStub;
+
+
 
 @Disabled("Only used for acceptance test")
 public class AcceptanceTest {
-    private GameController gameController;
-    private GameStateDTO gameState;
+    private GameController gc;
+    private GameStateDTO gs;
+    private PlayerController pc;
+    private GUIController gui;
+    private FieldController fc;
+    Language dansk = new Language();
     @BeforeEach
-    public void beforeAll(){
-        GUIController guiController = new GUIController();
+    public void beforeEach(){
+        gui = new GUIController();
+        gc = new GameController();
+        gs = new GameStateDTO(gui);
+        pc = new PlayerController();
+        fc = new FieldController(dansk);
+
     }
     @Test
     public void AK5_6(){
         System.out.println("Test");
+    }
+
+
+    @Test
+    public void AK7(){
+        pc.addPlayer(0,"UFO","moneyTest",2);
+        pc.addPlayer(1,"UFO","dummy1",2);
+        pc.addPlayer(1,"UFO","dummy2",2);
+        gc.TakeTurn(pc.getPlayerById(0));
+
     }
 }
