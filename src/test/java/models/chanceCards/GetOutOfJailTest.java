2b96fe1acd9725f17c651e75f0a5a9b65f4a6073
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-06 14:36:21 +0100

    created GetOutOfJailTest

---
+++ src/test/java/models/chanceCards/GetOutOfJailTest.java
@@ -0,0 +1,40 @@
+package models.chanceCards;
+
+import controllers.FieldController;
+import controllers.GUIControllerStub;
+import controllers.PlayerController;
+import models.Language;
+import models.Player;
+import models.dto.GameStateDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetOutOfJailTest {
+    GameStateDTO gameState;
+    GetOutOfJail card;
+
+    @BeforeEach
+    void setUp() {
+        Player player1 = new Player(0,"Player1");
+        ArrayList<Player> otherPlayers = new ArrayList<>();
+        otherPlayers.add(new Player(1,"player2"));
+        gameState = new GameStateDTO(player1,otherPlayers);
+        gameState.setPlayerController(new PlayerController());
+        gameState.setGuiController(new GUIControllerStub());
+        gameState.setFieldController(new FieldController(new Language()));
+        card = new GetOutOfJail("card1", "");
+    }
+
+    @Test
+    @DisplayName("test the player gets the card")
+    void chanceEffect() {
+        card.chanceEffect(gameState);
+        Player activePlayer = gameState.getActivePlayer();
+        assertEquals(card, activePlayer.getGetOutOfJail());
+    }
+}
