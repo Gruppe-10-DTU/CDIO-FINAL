523b35c6d31bf90e9d8ad4b4cad3e0b78fe5a305
sand4570 <sand4570@stud.kea.dk>
2023-01-09 10:29:30 +0100

    refactor on ferry

--- src/main/java/models/fields/Ferry.java
+++ src/main/java/models/fields/Ferry.java
@@ -7,10 +7,6 @@ import org.apache.commons.lang.NotImplementedException;
 public class Ferry extends Property{
 
     private int[] rent = new int[4];
-    private int rent0;
-    private int rent1;
-    private int rent2;
-    private int rent3;
 
     public int[] getRent() {
         return rent;
@@ -20,39 +16,6 @@ public class Ferry extends Property{
         this.rent[index] = rentAmound;
     }
 
-
-    public void setRent0(int rent0) {
-        this.rent0 = rent0;
-    }
-
-    public void setRent1(int rent1) {
-        this.rent1 = rent1;
-    }
-
-    public void setRent2(int rent2) {
-        this.rent2 = rent2;
-    }
-
-    public void setRent3(int rent3) {
-        this.rent3 = rent3;
-    }
-
-
-    public int getRent0() {
-        return rent0;
-    }
-
-    public int getRent1() {
-        return rent1;
-    }
-
-    public int getRent2() {
-        return rent2;
-    }
-
-    public int getRent3() {
-        return rent3;
-    }
 
     @Override
     public GameStateDTO fieldEffect(GameStateDTO gameState) {
