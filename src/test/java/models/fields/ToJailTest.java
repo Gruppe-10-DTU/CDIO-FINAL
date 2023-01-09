dd8fc80c642bc37560183cc3fa6522f87b8e968a
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-09 11:26:04 +0100

    created ToJailtest

---
+++ src/test/java/models/fields/ToJailTest.java
@@ -0,0 +1,20 @@
+package models.fields;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToJailTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("Check that the player is sent to jail")
+    void fieldEffect() {
+    }
+}
