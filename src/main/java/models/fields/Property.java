a575f423ef122ee2cd9314647b75251156a17404
NilasThoegersen <nilas96@gmail.com>
2023-01-10 14:45:43 +0100

    Added auction function in property for selling it.

--- src/main/java/models/fields/Property.java
+++ src/main/java/models/fields/Property.java
@@ -1,6 +1,8 @@
 package models.fields;
 
 import models.Player;
+import models.dto.GameStateDTO;
+import org.apache.commons.lang.ArrayUtils;
 
 public abstract class Property extends Field{
     protected int price;
@@ -16,4 +18,21 @@ public abstract class Property extends Field{
 
     public Player getOwner() {return owner;}
     public void setOwner(Player owner) {this.owner = owner;}
+
+    protected void auction(GameStateDTO gameStateDTO){
+        Player[] players = gameStateDTO.getPlayerController().getPlayers();
+        int index = (ArrayUtils.indexOf(players,gameStateDTO.getActivePlayer())+ 1) % players.length;
+        int k= index;
+        int bid = 0;
+        {
+            if(gameStateDTO.getGuiController().getUserLeftButtonPressed(players[index].getIdentifier() + " do you want to bid?", "yes", "no")){
+                bid = gameStateDTO.getGuiController().getBid("Hvor meget vil du byde? Sidste bud var " + bid, bid, players[index].getBalance());
+                k=index;
+            }
+            index = (index + 1) % players.length;
+        }while(k!=index);
+        players[index].setBalance(-this.price);
+        this.owner = players[index];
+
+    }
 }
