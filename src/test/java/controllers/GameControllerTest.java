1e7bdff5e98ddd15582fa7bde7860c4bbeb2801a
NilasThoegersen <nilas96@gmail.com>
2023-01-11 09:40:01 +0100

    Removed commented out code

--- src/test/java/controllers/GameControllerTest.java
+++ src/test/java/controllers/GameControllerTest.java
@@ -22,63 +22,6 @@ class GameControllerTest {
     Language language;
     Deck deck;
     GameStateDTO gameState;
-   /* @Test
-    void testGetOutOfJail(){
-        Player testPlayer = new Player(0, "Test");
-        //Set player in jail
-        testPlayer.setLocation(6);
-        //Make sure the next roll is 0
-        diceHolder.setNextRoll(6);
-        fieldController.jailPlayer(testPlayer);
-        gameController.TakeTurn(testPlayer);
-        assertEquals(18, testPlayer.getBalance());
-        assertEquals(12, testPlayer.getLocation());
-        assertFalse(((Jail) fieldController.getField(6)).isInJail(testPlayer));
-    }
-
-    */
-/*
-    @Test
-    void testFreeChoiceProperty() {
-        Player testPlayer = new Player(0, "Test");
-        assertNotEquals(testPlayer, ((Street) fieldController.getField(23)).getOwner());
-        gameController.characterSpecific(testPlayer);
-        assertEquals(testPlayer, ((Street) fieldController.getField(23)).getOwner());
-    }
-/*
-    @Test
-    void testBuyFromOtherPlayerChanceCard() {
-        Player testPlayer1 = new Player(0, "Test1");
-        Player testPlayer2 = new Player(0, "Test2");
-        Street[] properties = fieldController.getFreeFields();
-        for (Street property : properties
-             ) {
-            property.setOwner(testPlayer2);
-        }
-        //Testing if player 2 buys a property from player 1
-        gameController.characterSpecific(testPlayer1);
-        //Test if balance was updated correctly
-        assertEquals(25, testPlayer2.getBalance());
-        assertEquals(15, testPlayer1.getBalance());
-
-        //Test if ownership was transfered
-        assertNotEquals(testPlayer2, ((Street) fieldController.getField(23)).getOwner());
-    }
-
- */
-/*
-    @Test
-    void testNoMoreHouses() {
-        Player testPlayer1 = new Player(0, "Test1");
-        while(testPlayer1.decreaseSoldSign()){
-
-        }
-        gameController.characterSpecific(testPlayer1);
-        assertNotEquals(testPlayer1, ((Street) fieldController.getField(23)).getOwner());
-
-    }
-
- */
 
     @BeforeEach
     void setUp() {
@@ -99,15 +42,6 @@ class GameControllerTest {
 
         gameController = new GameController(gameState, language, deck);
     }
-    /*
-    @Test
-    void testEqualBalance() {
-        fieldController.setOwner(pc.getPlayers()[0],1);
-        assertEquals(pc.getPlayers()[0].getIdentifier(), gameController.checkAllBalance());
-    }
-
-     */
-
 
     @Test
     void win() {
