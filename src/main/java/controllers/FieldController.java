dff3531a468c27e385180d8698344ec22b80e884
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-13 10:16:24 +0100

    added null check of owner to housesAndHotelsOwned

--- src/main/java/controllers/FieldController.java
+++ src/main/java/controllers/FieldController.java
@@ -375,7 +375,7 @@ public class FieldController {
         int houses = 0;
         int hotels = 0;
         for (Field field : fieldArrayList) {
-            if (field instanceof Street && ((Street) field).getOwner().equals(player)){
+            if (field instanceof Street && ((Street) field).getOwner() != null &&((Street) field).getOwner().equals(player)){
                 if (((Street) field).isHotel()) hotels++;
                 else houses += ((Street) field).getHouseAmount();
             }
