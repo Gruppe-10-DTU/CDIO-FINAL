e708c3af209429f94d885574f73e50a4dc8fb533
Philip Astrup Cramer <s224319@dtu.dk>
2023-01-13 12:00:59 +0100

    added message to player to delay chanceEffect execution and allow player to keep up with the game

--- src/main/java/models/fields/Chance.java
+++ src/main/java/models/fields/Chance.java
@@ -8,6 +8,7 @@ public class Chance extends Field {
 
     @Override
     public GameStateDTO fieldEffect(GameStateDTO gameState, int rentMultiplier){
+        gameState.getGuiController().displayMsg("Du er landet på Prøv Lykken, og må derfor trække et kort.");
         ChanceCard drawnCard = gameState.getChancecardDeck().drawCard();
         drawnCard.chanceEffect(gameState);
         return gameState;
